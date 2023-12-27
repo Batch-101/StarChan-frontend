d1f7692c061c5ca7f1242daaa2bf73865595e979
Killyan <killyan.faucon@hotmail.fr>
2023-12-27 03:29:11 +0100

    tooltip double display bug correction

--- pages/profile.js
+++ pages/profile.js
@@ -241,7 +241,7 @@ function ProfilePage() {
                         maxFileSize={4500000}
                         minFileSize={0}
                         clickable>
-                        <img src={imgFile ? imgFile.preview.url : profilePicture} alt="profileImg" />
+                        <img data-tooltip-id="imgTooltip" src={imgFile ? imgFile.preview.url : profilePicture} alt="profileImg" />
                         <FontAwesomeIcon icon={faPen} className={styles.penIcon} />
                     </Files>
                     <Tooltip id="imgTooltip" style={{ maxWidth: 450, backgroundColor: "#391c4d", opacity: 1, color: "#ebe7c3" }} content="Cliquez ou glissez votre image (formats autorisés: .png, .jpg, .jpeg, max 4.5Mo)" />
